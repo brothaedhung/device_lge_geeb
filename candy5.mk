60a9749e4b6f4b33f79bff15769542e97c01d914
edgar.benson <edbenson@hotmail.com>
2014-12-09 20:02:58 -0800

    enable candykat build

---
+++ candy5.mk
@@ -0,0 +1,6 @@
+$(call inherit-product, device/lge/geeb/full_geeb.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/candy5/config/common_full_phone.mk)
+
+PRODUCT_NAME := candy5_geeb
